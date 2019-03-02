9bdd4c1ae413578b9dc62d5d18dd285c69c1da4f
LachlanMarnham <lachlan.marnham@gmail.com>
2019-03-02 02:30:05 +0000

    Create example_robots_txt fixture by reading from file

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,6 @@ import pytest
 
 @pytest.fixture(scope="module")
 def example_robots_txt():
-    return """User-agent: *
-Disallow: /asdfasdf.html
-Disallow: /pics/lisdad/
-Disallow: */restrict*?wghky
-Disallow: */restrict*&wghky
-Allow: */restrict*?pg=
-
-Sitemap: https://www.example.com/sitemap.xml
-
-User-agent: AdsBot-Google-Mobile-Apps
-Allow: /intl/"""
+    with open('test_data/robots.txt') as file:
+        contents = file.read()
+    return contents
